1b4176ad54b7e08b36fcef862d86d8f645c19d91
Ítalo Penha <italopenha77@outlook.com>
2023-11-23 20:44:57 -0300

    Criada a classe Posicao

--- Program.cs
+++ Program.cs
@@ -1,10 +1,14 @@
-﻿namespace XadrezConsole
+﻿using Tabuleiro;
+
+namespace XadrezConsole
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Posicao P = new Posicao(3, 4);
+
+            Console.WriteLine(P);
         }
     }
 }
