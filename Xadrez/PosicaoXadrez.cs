0c1ab45bfe853f3be2be67f9ed3cd45a41da2341
Ítalo Penha <italopenha77@outlook.com>
2023-11-24 12:35:21 -0300

    Criada a classe PosicaoXadrez

---
+++ Xadrez/PosicaoXadrez.cs
@@ -0,0 +1,28 @@
+﻿using XadrezConsole.Tabuleiro;
+
+namespace XadrezConsole.Xadrez
+{
+    internal class PosicaoXadrez
+    {
+        public char Coluna { get; set; }
+        public int Linha { get; set; }
+
+        public PosicaoXadrez() { }
+
+        public PosicaoXadrez(char coluna, int linha)
+        {
+            Coluna = coluna;
+            Linha = linha;
+        }
+
+        public Posicao ToPosicao()
+        {
+            return new Posicao(8 - Linha, Coluna - 'a');
+        }
+
+        public override string ToString()
+        {
+            return $"{Coluna}{Linha}";
+        }
+    }
+}
