8327807004ee013b5579a25d718264bb0da47133
Ítalo Penha <italopenha77@outlook.com>
2023-11-23 21:37:14 -0300

    Criada a classe Peca

---
+++ Tabuleiro/Peca.cs
@@ -0,0 +1,20 @@
+﻿namespace XadrezConsole.Tabuleiro
+{
+    internal class Peca
+    {
+        public Posicao Posicao { get; set; }
+        public Cor Cor { get; protected set; }
+        public int QteMovimentos { get; protected set; }
+        public Quadro Quadro { get; protected set; }
+
+        public Peca() { }
+
+        public Peca(Posicao posicao, Cor cor, Quadro quadro)
+        {
+            Posicao = posicao;
+            Cor = cor;
+            QteMovimentos = 0;
+            Quadro = quadro;
+        }
+    }
+}
