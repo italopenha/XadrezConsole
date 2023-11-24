32d25a7089f2dda577f50c336fd0cd7af5052af3
Ítalo Penha <italopenha77@outlook.com>
2023-11-24 12:49:34 -0300

    Melhorada a implementação do método ImprimirQuadro

--- Tela.cs
+++ Tela.cs
@@ -8,6 +8,7 @@ namespace XadrezConsole
         {
             for (int i = 0; i < quadro.Linhas; i++)
             {
+                Console.Write($"{8 - i} ");
                 for (int j = 0; j < quadro.Colunas; j++)
                 {
                     if (quadro.Peca(i, j) == null)
@@ -16,10 +17,27 @@ namespace XadrezConsole
                     }
                     else
                     {
-                        Console.Write($"{quadro.Peca(i, j)} ");
+                        ImprimirPeca(quadro.Peca(i, j));
+                        Console.Write(" ");
                     }
                 }
                 Console.WriteLine();
+            }
+            Console.WriteLine("  A B C D E F G H");
+        }
+
+        public static void ImprimirPeca(Peca peca)
+        {
+            if (peca.Cor == Cor.Branca)
+            {
+                Console.Write(peca);
+            }
+            else
+            {
+                ConsoleColor aux = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write(peca);
+                Console.ForegroundColor = aux;
             }
         }
     }
