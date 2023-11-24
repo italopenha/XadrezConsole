8d0278f23fe7c817672041a53d93f5875202b003
Ítalo Penha <italopenha77@outlook.com>
2023-11-23 21:58:42 -0300

    Criada a classe Tela

---
+++ Tela.cs
@@ -0,0 +1,26 @@
+﻿using XadrezConsole.Tabuleiro;
+
+namespace XadrezConsole
+{
+    internal class Tela
+    {
+        public static void ImprimirQuadro(QuadroDeJogo quadro)
+        {
+            for (int i = 0; i < quadro.Linhas; i++)
+            {
+                for (int j = 0; j < quadro.Colunas; j++)
+                {
+                    if (quadro.Peca(i, j) == null)
+                    {
+                        Console.Write("- ");
+                    }
+                    else
+                    {
+                        Console.Write($"{quadro.Peca(i, j)} ");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
