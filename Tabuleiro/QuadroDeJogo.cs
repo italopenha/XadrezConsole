36b2a27fb1f0098cc2cd27a7e57ddd899f2adc9d
Ítalo Penha <italopenha77@outlook.com>
2023-11-23 21:40:07 -0300

    Criada a classe QuadroDeJogo

---
+++ Tabuleiro/QuadroDeJogo.cs
@@ -0,0 +1,18 @@
+﻿namespace XadrezConsole.Tabuleiro
+{
+    internal class QuadroDeJogo
+    {
+        public int Linhas { get; set; }
+        public int Colunas { get; set; }
+        private Peca[,] Pecas;
+
+        public QuadroDeJogo() { }
+
+        public QuadroDeJogo(int linhas, int colunas)
+        {
+            Linhas = linhas;
+            Colunas = colunas;
+            Pecas = new Peca[Linhas, Colunas];
+        }
+    }
+}
