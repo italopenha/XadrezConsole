7de5dabcec544c906cc3b1e3e648e18938061eeb
Ítalo Penha <italopenha77@outlook.com>
2023-11-23 22:26:23 -0300

    Criadas as classes Rei e Torre

---
+++ Xadrez/Rei.cs
@@ -0,0 +1,17 @@
+﻿using XadrezConsole.Tabuleiro;
+
+namespace XadrezConsole.Xadrez
+{
+    internal class Rei : Peca
+    {
+        public Rei(Cor cor, QuadroDeJogo quadro) : base(cor, quadro)
+        {
+
+        }
+
+        public override string ToString()
+        {
+            return "R";
+        }
+    }
+}
---
+++ Xadrez/Torre.cs
@@ -0,0 +1,17 @@
+﻿using XadrezConsole.Tabuleiro;
+
+namespace XadrezConsole.Xadrez
+{
+    internal class Torre : Peca
+    {
+        public Torre(Cor cor, QuadroDeJogo quadro) : base(cor, quadro)
+        {
+
+        }
+
+        public override string ToString()
+        {
+            return "T";
+        }
+    }
+}
