fce58d5de8b7b9f67e63633e9bc13c4aa15f7160
Ítalo Penha <italopenha77@outlook.com>
2023-11-24 19:07:10 -0300

    Criados métodos de movimento das classes Rei e Torre

--- Xadrez/Rei.cs
+++ Xadrez/Rei.cs
@@ -9,6 +9,76 @@ namespace XadrezConsole.Xadrez
 
         }
 
+        private bool PodeMover(Posicao pos)
+        {
+            Peca p = Quadro.Peca(pos);
+            return p == null || p.Cor != Cor;
+        }
+
+        public override bool[,] MovimentosPossiveis()
+        {
+            bool[,] mat = new bool[Quadro.Linhas, Quadro.Colunas];
+
+            Posicao pos = new Posicao(0, 0);
+
+            // norte
+            pos.DefinirValores(Posicao.Linha - 1, Posicao.Coluna);
+            if (Quadro.PosicaoValida(pos) && PodeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+            }
+
+            // nordeste
+            pos.DefinirValores(Posicao.Linha - 1, Posicao.Coluna + 1);
+            if (Quadro.PosicaoValida(pos) && PodeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+            }
+
+            // leste
+            pos.DefinirValores(Posicao.Linha, Posicao.Coluna + 1);
+            if (Quadro.PosicaoValida(pos) && PodeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+            }
+
+            // sudeste
+            pos.DefinirValores(Posicao.Linha + 1, Posicao.Coluna + 1);
+            if (Quadro.PosicaoValida(pos) && PodeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+            }
+
+            // sul
+            pos.DefinirValores(Posicao.Linha + 1, Posicao.Coluna);
+            if (Quadro.PosicaoValida(pos) && PodeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+            }
+
+            // sudoeste
+            pos.DefinirValores(Posicao.Linha + 1, Posicao.Coluna - 1);
+            if (Quadro.PosicaoValida(pos) && PodeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+            }
+
+            // oeste
+            pos.DefinirValores(Posicao.Linha, Posicao.Coluna - 1);
+            if (Quadro.PosicaoValida(pos) && PodeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+            }
+
+            // noroeste
+            pos.DefinirValores(Posicao.Linha - 1, Posicao.Coluna - 1);
+            if (Quadro.PosicaoValida(pos) && PodeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+            }
+            return mat;
+        }
+
         public override string ToString()
         {
             return "R";
--- Xadrez/Torre.cs
+++ Xadrez/Torre.cs
@@ -9,6 +9,68 @@ namespace XadrezConsole.Xadrez
 
         }
 
+        private bool PodeMover(Posicao pos)
+        {
+            Peca p = Quadro.Peca(pos);
+            return p == null || p.Cor != Cor;
+        }
+
+        public override bool[,] MovimentosPossiveis()
+        {
+            bool[,] mat = new bool[Quadro.Linhas, Quadro.Colunas];
+
+            Posicao pos = new Posicao(0, 0);
+
+            // norte
+            pos.DefinirValores(Posicao.Linha - 1, Posicao.Coluna);
+            while (Quadro.PosicaoValida(pos) && PodeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+                if (Quadro.Peca(pos) != null && Quadro.Peca(pos).Cor != Cor)
+                {
+                    break;
+                }
+                pos.Linha = pos.Linha - 1;
+            }
+
+            // sul
+            pos.DefinirValores(Posicao.Linha + 1, Posicao.Coluna);
+            while (Quadro.PosicaoValida(pos) && PodeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+                if (Quadro.Peca(pos) != null && Quadro.Peca(pos).Cor != Cor)
+                {
+                    break;
+                }
+                pos.Linha = pos.Linha + 1;
+            }
+
+            // leste
+            pos.DefinirValores(Posicao.Linha, Posicao.Coluna + 1);
+            while (Quadro.PosicaoValida(pos) && PodeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+                if (Quadro.Peca(pos) != null && Quadro.Peca(pos).Cor != Cor)
+                {
+                    break;
+                }
+                pos.Coluna = pos.Coluna + 1;
+            }
+
+            // oeste
+            pos.DefinirValores(Posicao.Linha, Posicao.Coluna - 1);
+            while (Quadro.PosicaoValida(pos) && PodeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+                if (Quadro.Peca(pos) != null && Quadro.Peca(pos).Cor != Cor)
+                {
+                    break;
+                }
+                pos.Coluna = pos.Coluna - 1;
+            }
+            return mat;
+        }
+
         public override string ToString()
         {
             return "T";
