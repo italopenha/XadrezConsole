68755c4503846818cd0d9f2597434aa55a53de90
Ítalo Penha <italopenha77@outlook.com>
2023-11-24 20:11:42 -0300

    Classe Program atualizada

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿using Tabuleiro;
+﻿using XadrezConsole.Tabuleiro;
+using XadrezConsole.Tabuleiro.Exceptions;
+using XadrezConsole.Xadrez;
 
 namespace XadrezConsole
 {
@@ -6,9 +8,50 @@ namespace XadrezConsole
     {
         static void Main(string[] args)
         {
-            Posicao P = new Posicao(3, 4);
+            try
+            {
+                PartidaDeXadrez partida = new PartidaDeXadrez();
 
-            Console.WriteLine(P);
+                while (!partida.Terminada)
+                {
+                    try
+                    {
+                        Console.Clear();
+                        Tela.ImprimirQuadro(partida.Quadro);
+                        Console.WriteLine();
+                        Console.WriteLine($"Turno: {partida.Turno}");
+                        Console.WriteLine($"Aguardando jogada: {partida.JogadorAtual}");
+
+                        Console.WriteLine();
+                        Console.Write("Origem: ");
+                        Posicao origem = Tela.LerPosicaoXadrez().ToPosicao();
+                        partida.ValidarPosicaoDeOrigem(origem);
+
+                        bool[,] posicoesPossiveis = partida.Quadro.Peca(origem).MovimentosPossiveis();
+
+                        Console.Clear();
+                        Tela.ImprimirQuadro(partida.Quadro, posicoesPossiveis);
+
+                        Console.WriteLine();
+                        Console.Write("Destino: ");
+                        Posicao destino = Tela.LerPosicaoXadrez().ToPosicao();
+                        partida.ValidarPosicaoDeDestino(origem, destino);
+
+                        partida.RealizaJogada(origem, destino);
+                    }
+                    catch (TabuleiroException e)
+                    {
+                        Console.WriteLine(e.Message);
+                        Console.ReadLine();
+                    }
+                }
+
+                Tela.ImprimirQuadro(partida.Quadro);
+            }
+            catch (TabuleiroException e)
+            {
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
